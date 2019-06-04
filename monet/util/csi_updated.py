102e6adf727825893947f04bb321ae536fe5378e
aring1988 <aring1@umd.edu>
2019-06-04 11:21:55 -0400

    added array expand/trim functions

--- monet/util/csi_updated.py
+++ monet/util/csi_updated.py
@@ -2,57 +2,50 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 from math import *
 import sys 
-#from scipy.io import netcdf
-#from pylab import *
 import numpy as np
 import matplotlib.pyplot as plt
 import string
 import datetime
-#import shapely.geometry as sgeo
-#from hysplit import *
 from itertools import permutations
 import time
-
+from xarray import DataArray as da
 
 """
 routines to help calculates critical success index by a point by point comparison.
 data must be gridded the same.
 Functions
 ---------
-match_ra
+match_arrays
 get_area_ra
 calc_fss  (fraction skill score)
 find_threshold
 mask_threshold
-calc_csi (critical success index, aka figure of merit in space)
-"""
-def match_ra(ra1, lat1, lon1, ra2, lat2, lon2, missing_value=0, verbose=1):
-    """inputs two data 'structures' data, latitude, longitude.
-    ra1 : numpy array 
-    ra2 : numpy array 
-    lon1 : numpy array 
-    lat1 : numpy array 
-    lon2 : numpy array 
-    lat2 : numpy array 
-    shape of ra1, lat1, lon1 should be the same
-    shape of ra2, lat2, lon2 should be the same.
-    Assumes lat-lon grids are spaced the same but may be offset. 
-    output arrays can be input into calc_csi 
-    return
-    latlongrid 
-    ra1 
-    ra2
+expand_array
+expand_both
+trim_array
+trim_both
+calc_csi (critical success index, aka figure of merit in space)"""
+
+def match_arrays(ra1, lat1, lon1, ra2, lat2, lon2, missing_value = 0, verbose = 0):
+    """inputs two arrays - adds padding zeros to make arrays fully overlap
+    calls for regridding - MONET tool
+    output arrays can be input into calc_csi, calc_fss
+    ra1: xarray (data)
+    lat1: xarray (latitude)
+    lon1: xarray (longitude)
+    ra2: xarray (data)
+    lat2: xarray (latitude)
+    lon2: xarray (longitude)
+    missing value: 0 (default)
+    verbose: 0 (default)
+    return ra1new, lat1new, lon1new, ra2new, lat2new, lon2new
     """
-    latlongrid=[]
     if verbose==1: 
-        print('RA1 ' , ra1.shape , '\n LAT1 ' , lat1.shape , '\n LON1 ', lon1.shape)
-        print('RA2 ' , ra2.shape , '\n LAT2 ' , lat2.shape , '\n LON2 ', lon2.shape)
-    ##check to make shapes of input arrays are the same.
-    if ra1.shape == lat1.shape and ra1.shape == lat1.shape and  \
-       ra2.shape == lat2.shape and ra2.shape == lat2.shape:
-        
+        print('RA1 ' , ra1.shape)
+        print('RA2 ' , ra2.shape)
+            
        ##check to make sure data array not already equal. 
-       if not (np.array_equal(lon1, lon2) and np.array_equal(lat1, lat2)):
+        if not (np.array_equal(lon1, lon2) and np.array_equal(lat1, lat2)):
           
            ###MATCH LATITUDES######## 
            lat_space =  round(lat1[1][0] - lat1[0][0], 3)
@@ -141,7 +134,7 @@ def match_ra(ra1, lat1, lon1, ra2, lat2, lon2, missing_value=0, verbose=1):
                ra2=np.concatenate((tempra.T, ra2), axis=1)
            #print 'RA2 \n' , ra2
            latlongrid = np.meshgrid(lonlist, latlist)
-       else:
+        else:
            latlongrid = np.meshgrid(lon1, lat1)    
            #print latlongrid[0]
            #print latlongrid[1]
@@ -288,37 +281,182 @@ def find_threshold(ra1, ra2, nodata_value=None):
     return list2[numpixels]
 
 def mask_threshold(ra1, threshold = 0):
-    """input array and threshold. 
-        Returns array with 1's where value is above threshold, and 0 otherwise"""
+    """Input array and threshold. 
+    Returns array with 1's where value is above threshold, and 0 otherwise"""
     vp1 = np.where(ra1 > threshold)
     mask1 = np.zeros_like(ra1)
     mask1[vp1] = 1
     return mask1
 
-def trim_arrays(mask1, mask2, sn=[0,5]):
-    """Trims binary mask arrays to array size that encompasses data and minor
-    padding for both mask input arrays. Requires pixel radius (sn) value to determine 
-    padding to keep in array. Will reduce time of fss calculation."""
+def expand_array(array, pad = 5):
+    """Input xarray data array and desired radius of padding.
+    Default padding is 10 indices on all 4 sides of array.
+    Returns data array with latitudes and longitudes
+    of padded array."""
+    #Calculating total number of padded indexes
+    pads = pad * 2
+    #Extracting 1D arrays of latitude and longitude values
+    rows = np.array(array.latitude[:,0])
+    cols = np.array(array.longitude[0,:])
+    ydim = len(rows)
+    xdim = len(cols)
+    #Calculating lat and lon deltas, start and end lat/lon values
+    #To generate new 1D arrays of expanded dimensions
+    dlat = rows[1] - rows[0]
+    slat = rows[0] - (dlat * pads)
+    elat = rows[-1] + (dlat * (pads + 1))
+    newrows = np.arange(slat, elat, dlat)
+    dlon = cols[1] - cols[0]
+    slon = cols[0] - (dlon * pads)
+    elon = cols[-1] + (dlon * (pads + 1))
+    newcols= np.arange(slon, elon, dlon)
+    #Creating new lat/lon arrays 
+    newlat, newlon = np.meshgrid(newrows, newcols, indexing = 'ij')
+    #Creating temporary array of zeros (expanded dimensions)
+    data = np.zeros((ydim + (pads * 2), xdim + (pads * 2)))
+    #Inserting data into new expanded array
+    data[pads:(pads + ydim), pads:(pads + xdim)] = array
+    #Converting new array to xarray (assigning coordinates)
+    expanded = da(data)
+    expanded = expanded.rename({'dim_0': 'y'})
+    expanded = expanded.rename({'dim_1': 'x'})
+    newlat = da(newlat)
+    newlat = newlat.rename({'dim_0': 'y'})
+    newlat = newlat.rename({'dim_1': 'x'})
+    newlon = da(newlon)
+    newlon = newlon.rename({'dim_0': 'y'})
+    newlon = newlon.rename({'dim_1': 'x'})
+    expanded = expanded.assign_coords(latitude = newlat)
+    expanded = expanded.assign_coords(longitude = newlon)    
+    return expanded
 
-    pad = sn[1]*2
+def expand_both(array1, array2, pad = 3):
+    """Input xarray data arrays (1 and 2), padding radius.
+    Determines largest common grid for the two arrays.
+    Adds designated padding around data.
+    Necessary step before regridding can occur.
+    Returns two arrays with latitudes and longitudes."""
+    pads = pad * 2
+    #Determining 1D lat/lon values for each array
+    row1 = np.array(array1.latitude[:,0])
+    col1 = np.array(array1.longitude[0,:])
+    row2 = np.array(array2.latitude[:,0])
+    col2 = np.array(array2.longitude[0,:])
+    #Creating arrays of min/max lat/lon values for both arrays
+    yedge = [min(row1), min(row2), max(row1), max(row2)]
+    xedge = [min(col1), min(col2), max(col1), max(col2)]
+    dlat1 = row1[1] - row1[0]
+    dlon1 = col1[1] - col1[0]
+    dlat2 = row2[1] - row2[0]
+    dlon2 = col2[1] - col2[0]
+    #Determine expanded longitudes for both arrays 
+    slon1 = min(col1) - abs(dlon1 * (abs(ceil((min(xedge) - min(col1)) / dlon1)) + pads))
+    elon1 = max(col1) + abs(dlon1 * (abs(ceil((max(xedge) - max(col1)) / dlon1)) + pads))
+    slon2 = min(col2) - abs(dlon2 * (abs(ceil((min(xedge) - min(col2)) / dlon2)) + pads))
+    elon2 = max(col2) + abs(dlon2 * (abs(ceil((max(xedge) - max(col2)) / dlon2)) + pads))
+    #Determine expanded latitudes for both arrays
+    slat1 = min(row1) - abs(dlat1 * (abs(ceil((min(yedge) - min(row1)) / dlat1)) + pads))
+    elat1 = max(row1) + abs(dlat1 * (abs(ceil((max(yedge) - max(row1)) / dlat1)) + pads))
+    slat2 = min(row2) - abs(dlat2 * (abs(ceil((min(yedge) - min(row2)) / dlat2)) + pads))
+    elat2 = max(row2) + abs(dlat2 * (abs(ceil((max(yedge) - max(row2)) / dlat2)) + pads))
+    #Determine orientation of original array  - maintain start and end for new array
+    if abs(col2[0] - elon1) < abs(col1[0] - slon1):
+        tmp = slon1
+        slon1 = elon1
+        elon1 = tmp
+    if abs(col2[0] - elon2) < abs(col2[0] - slon2):
+        tmp = slon2
+        slon2 = elon2
+        elon2 = tmp
+    if abs(row1[0] - elat1) < abs(row1[0] - slat1):
+        tmp = slat1
+        slat1 = elat1
+        elat1 = tmp
+    if abs(row2[0] - elat2) < abs(row2[0] - slat2):
+        tmp = slat2
+        slat2 = elat2
+        elat2 = tmp
+    newcols1 = np.arange(slon1, elon1, dlon1)
+    newcols2 = np.arange(slon2, elon2, dlon2)
+    newrows1 = np.arange(slat1, elat1, dlat1)
+    newrows2 = np.arange(slat2, elat2, dlat2)
+    #Creating new lat/lon arrays 
+    newlat1, newlon1 = np.meshgrid(newrows1, newcols1, indexing = 'ij')
+    newlat2, newlon2 = np.meshgrid(newrows2, newcols2, indexing = 'ij')
+    #Creating temporary array of zeros (expanded dimensions)
+    data1 = np.zeros((len(newrows1), len(newcols1)))
+    data2 = np.zeros((len(newrows2), len(newcols2)))
+    #Inserting data into new expanded array
+    stlat1 = (abs(newrows1 - row1[0])).argmin()
+    stlon1 = (abs(newcols1 - col1[0])).argmin()
+    stlat2 = (abs(newrows2 - row2[0])).argmin()
+    stlon2 = (abs(newcols2 - col2[0])).argmin()
+    data1[stlat1:(stlat1 + len(row1)), stlon1:(stlon1 + len(col1))] = array1
+    data2[stlat2:(stlat2 + len(row2)), stlon2:(stlon2 + len(col2))] = array2
+    #Converting new array to xarray (assigning coordinates)
+    expanded1 = da(data1)
+    expanded2 = da(data2)
+    expanded1 = expanded1.rename({'dim_0': 'y'})
+    expanded1 = expanded1.rename({'dim_1': 'x'})
+    expanded2 = expanded2.rename({'dim_0': 'y'})
+    expanded2 = expanded2.rename({'dim_1': 'x'})
+    newlat1 = da(newlat1)
+    newlon1 = da(newlon1)
+    newlat2 = da(newlat2)
+    newlon2 = da(newlon2)
+    newlat1 = newlat1.rename({'dim_0': 'y'})
+    newlat1 = newlat1.rename({'dim_1': 'x'})
+    newlon1 = newlon1.rename({'dim_0': 'y'})
+    newlon1 = newlon1.rename({'dim_1': 'x'})
+    newlat2 = newlat2.rename({'dim_0': 'y'})
+    newlat2 = newlat2.rename({'dim_1': 'x'})
+    newlon2 = newlon2.rename({'dim_0': 'y'})
+    newlon2 = newlon2.rename({'dim_1': 'x'})
+    expanded1 = expanded1.assign_coords(latitude = newlat1)
+    expanded1 = expanded1.assign_coords(longitude = newlon1)
+    expanded2 = expanded2.assign_coords(latitude = newlat2)
+    expanded2 = expanded2.assign_coords(longitude = newlon2)
+    return expanded1, expanded2
+
+def trim_array(array, pad = 5):
+    """Trims down array to array size that encompasses 
+    data and minor padding for input array.
+    Inputs: array to trim, radius of padding around array."""  
+    pad = pad * 2
+    rows = da.any(array, axis = 1)
+    cols = da.any(array, axis = 0)
+    ymin, ymax = np.where(rows)[0][[0,-1]]
+    xmin, xmax = np.where(cols)[0][[0,-1]]
+    #Trims array 
+    trim = array[(ymin - pad):(ymax + 1 + pad), (xmin - pad):(xmax + 1 + pad)]
+    return trim
+
+def trim_both(mask1, mask2, pad = 5):
+    """Trims two arrays to array size 
+    that encompasses data and minor
+    padding for both input arrays. 
+    Requires pixel radius (sn) value to determine 
+    padding to keep in array. 
+    Inputs: array1, array2, radius of padding around array."""
+    pad = pad * 2
     print(pad)
-    rows1 = np.any(mask1, axis=1)
-    cols1 = np.any(mask1, axis=0)
-    rows2 = np.any(mask2, axis=1)
-    cols2 = np.any(mask2, axis=0)
+    rows1 = np.any(mask1, axis = 1)
+    cols1 = np.any(mask1, axis = 0)
+    rows2 = np.any(mask2, axis = 1)
+    cols2 = np.any(mask2, axis = 0)
     ymin1, ymax1 = np.where(rows1)[0][[0, -1]]
     xmin1, xmax1 = np.where(cols1)[0][[0, -1]]
     ymin2, ymax2 = np.where(rows2)[0][[0, -1]]
     xmin2, xmax2 = np.where(cols2)[0][[0, -1]]
-    trim1 = mask1[(ymin1-pad):(ymax1+1+pad), (xmin1-pad):(xmax1+1+pad)]
-    trim2 = mask2[(ymin2-pad):(ymax2+1+pad), (xmin2-pad):(xmax2+1+pad)]
-
+    trim1 = mask1[(ymin1 - pad):(ymax1 + 1 + pad), (xmin1 - pad):(xmax1 + 1 + pad)]
+    trim2 = mask2[(ymin2 - pad):(ymax2 + 1 + pad), (xmin2 - pad):(xmax2 + 1 + pad)]
+    #Determines minimum yarr and xarr between the two arrays
     yarr = [ymin1, ymin2, ymax1, ymax2]
     xarr = [xmin1, xmin2, xmax1, xmax2]
-    trimboth1 = mask1[(min(yarr)-pad):(max(yarr)+1+pad), (min(xarr)-pad):(max(xarr)+1+pad)]
-    trimboth2 = mask2[(min(yarr)-pad):(max(yarr)+1+pad), (min(xarr)-pad):(max(xarr)+1+pad)]
-
-    return trim1, trim2, trimboth1, trimboth2
+    #Trims each array to size
+    trimboth1 = mask1[(min(yarr) - pad):(max(yarr) + 1 + pad), (min(xarr) - pad):(max(xarr) + 1 + pad)]
+    trimboth2 = mask2[(min(yarr) - pad):(max(yarr) + 1 + pad), (min(xarr) - pad):(max(xarr) + 1 + pad)]
+    return trimboth1, trimboth2
 
 def calc_csi(ra1, ra2, area=None, nodata_value='', threshold=0, verbose=0):
     """ra1 and ra2 need to be on the same grid. 
@@ -329,15 +467,9 @@ def calc_csi(ra1, ra2, area=None, nodata_value='', threshold=0, verbose=0):
         nodata_value = flag for expanded array grid cells created if ash near boundary
         threshold = value for data threshold
         CSI equation: hits / (hits + misses + false alarms)"""
-    #ra1 = ra1.load()
-    #ra2 = ra2.load()
     area=np.array(area)
     #Creating a csihash disctionary
     csihash = {}
-    #vptest = np.where(logical_and(ra1 > 0 , ra1 <= threshold))
-    #if (verbose == 1):
-    #    print('TEST VPTEST ' , threshold , vptest)
-
     #Converting ra1 and ra2 to arrays of 0's and 1's (1 with values, 0 no values)
     mask1 = mask_threshold(ra1, threshold = 0)
     if (threshold != 0):
@@ -374,7 +506,6 @@ def calc_csi(ra1, ra2, area=None, nodata_value='', threshold=0, verbose=0):
     print('ra1ave (obs)' ,  ra1ave) 
     print('ra2ave (calc)' , ra2ave, end=' ') 
     print('NORM' , norm , 'ra1corr*ra2corr' ,  (ra1corr *ra2corr).sum())
-#    print totalpts, mask1.shape
     print(mask1.sum(), mask2.sum(), np.max(ra1corr), np.min(ra1corr))
 
     ra1ave = 0
