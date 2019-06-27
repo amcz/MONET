32e8940a2c89394916a3dbaec7e93dec7d5e9c92
aring1988 <aring1@umd.edu>
2019-06-27 11:46:52 -0400

    JPSS ftp watch codes

--- monet/utilhysplit/watch_jpss_upload_os.py
+++ monet/utilhysplit/watch_jpss_upload_os.py
@@ -17,7 +17,8 @@ path_to_watch = '/pub/jpsss_upload/'
 #Creating list of current files in ftp folder
 current = os.listdir(path_to_watch)
 #Reading the list of files from the last iteration
-with open('/hysplit-users/allisonr/VOLCANO/original_list.txt', 'r') as fs:
+direct = '/hysplit-users/allisonr/VOLCANO/'
+with open(direct+'original_list.txt', 'r') as fs:
     original = json.loads(fs.read())
 #Determining what was added and what was removed
 added = [fs for fs in current if not fs in original]
@@ -41,9 +42,9 @@ if current == original:
     print('No updates to '+path_to_watch+' folder!')
 
 #Write current list of files to original_list.txt - update for next iteration
-with open('/hysplit-users/allisonr/VOLCANO/original_list2.txt', 'w') as fis:
+with open(direct+'/original_list2.txt', 'w') as fis:
     fis.write(json.dumps(current))
 
-os.system('mv original_list2.txt original_list.txt')
+os.system('mv '+direct+'original_list2.txt '+direct+'original_list.txt')
 
 
