19b32bc6d57bcc248fef89a6efe7b1d6b1d29a45
aring1988 <aring1@umd.edu>
2020-01-24 11:34:48 -0500

    minor adjustments to account for reading of symbols - reduce error

--- monet/util/read_csv.py
+++ monet/util/read_csv.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 #Opens CSV file and reads it into a Pandas DataFrame
 def open_file(fname,delimiter):
-    print(fname)
+    #print(fname)
     readCSV = pd.read_csv(fname, delimiter = delimiter, encoding = 'ISO-8859-1')
     return readCSV 
 
