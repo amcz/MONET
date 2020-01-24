e0032834db3f34e4acd5bf3bee801f4f53d88a80
aring1988 <aring1@umd.edu>
2020-01-24 11:43:43 -0500

    added check functions - modified code to ensure alerts are not missed

--- monet/utilhysplit/run_hysp_alert.py
+++ monet/utilhysplit/run_hysp_alert.py
@@ -6,7 +6,7 @@
 #and the trajectory is initiated again at the next call of this program
 
 def make_files( fpath, fname):
-    """ Makes CONTROL and SETUP files from information in
+    """ Makes CONTROL and SETUP files for Trajectory simulation from information in
     VOLCAT alert xml files pushed to the /pub/jpsss_upload/ ftp folder.
     
     Met Model set: GFS0p25
@@ -27,13 +27,16 @@ def make_files( fpath, fname):
 
     met_model = 'GFS0p25' 
     wdir = '/hysplit-users/allisonr/VOLCANO/Trajectory/'
+    wdir2 = '/hysplit-users/allisonr/VOLCANO/Dispersion/'
     duration = 36 #Forward: (+hours) Backward: (-hours)
+    duration2 = 18 #For dispersion run only
     topbound = 30000  #Top of model domain (m-agl)
     vertical = 0 # 0:data, 1:isob, 2:isen, 3:dens 4:sigma 5:diverg 6:msl2agl 7:avg
 
     alerts = volcalert.open_xml(fpath+fname)
     #Returns an 8 dimensional array
     data = volcalert.get_alert_values(alerts)
+    alert_type = data[1]
     start_lat = data[5]
     start_lon = data[6]
     #Returns the nearest volcanoes to the alert as list of 6 arrays
@@ -52,13 +55,19 @@ def make_files( fpath, fname):
     csv = usgs.open_file(fname = '/hysplit-users/allisonr/Alice/MONET/monet/data/usgs_table.csv',delimiter = ',')
     headers = usgs.find_headers(csv)
     csv[headers[4]] = csv[headers[4]].str.normalize('NFKD').str.encode('ascii', errors = 'ignore').str.decode('utf-8')
+    
     if csv[headers[4]].str.contains(volcname).any():
+        volcname2 = volcname
         this_volc = csv[csv[headers[4]].str.contains(volcname)]
         alt = this_volc[headers[12]].values[0]
     else:
-        volcname = 'Unknown'
+        volcname2 = 'Unknown'
         alt = 300.0
-    
+    if volcname.find('[') != -1:
+        volcname2 = volcname[:volcname.index('[')]
+        this_volc = csv[csv[headers[4]].str.contains(volcname2)]
+        alt = this_volc[headers[12]].values[0]
+
     #Find met fields necessary for the hysplit run
     #Two possible functions to gather appropriate met fields:
     #      findcycles_archive(datetime object start, datetime object end, 
@@ -83,16 +92,16 @@ def make_files( fpath, fname):
     # set the duration of the simulation (hours)
     control.run_duration = duration
     # set location of volcano
-    control.add_location(latlon=(start_lat, start_lon), alt = alt, rate = False, area = False)
-    control.add_location(latlon=(start_lat, start_lon), alt = alt+1000, rate = False, area = False)
-    control.add_location(latlon=(start_lat, start_lon), alt = alt+1500, rate = False, area = False)
-    control.add_location(latlon=(start_lat, start_lon), alt = alt+2500, rate = False, area = False)
-    control.add_location(latlon=(start_lat, start_lon), alt = alt+3000, rate = False, area = False)
-    control.add_location(latlon=(start_lat, start_lon), alt = alt+5000, rate = False, area = False)
-    control.add_location(latlon=(start_lat, start_lon), alt = alt+7500, rate = False, area = False)
-    control.add_location(latlon=(start_lat, start_lon), alt = alt+10000, rate = False, area = False)
-    control.add_location(latlon=(start_lat, start_lon), alt = alt+12500, rate = False, area = False)
-    control.add_location(latlon=(start_lat, start_lon), alt = alt+15000, rate = False, area = False)
+    control.add_location(latlon = (start_lat, start_lon), alt = alt, rate = False, area = False)
+    control.add_location(latlon = (start_lat, start_lon), alt = alt+1000, rate = False, area = False)
+    control.add_location(latlon = (start_lat, start_lon), alt = alt+1500, rate = False, area = False)
+    control.add_location(latlon = (start_lat, start_lon), alt = alt+2500, rate = False, area = False)
+    control.add_location(latlon = (start_lat, start_lon), alt = alt+3000, rate = False, area = False)
+    control.add_location(latlon = (start_lat, start_lon), alt = alt+5000, rate = False, area = False)
+    control.add_location(latlon = (start_lat, start_lon), alt = alt+7500, rate = False, area = False)
+    control.add_location(latlon = (start_lat, start_lon), alt = alt+10000, rate = False, area = False)
+    control.add_location(latlon = (start_lat, start_lon), alt = alt+12500, rate = False, area = False)
+    control.add_location(latlon = (start_lat, start_lon), alt = alt+15000, rate = False, area = False)
     control.vertical_motion = vertical
     control.ztop = topbound
     control.outdir = wdir
@@ -109,31 +118,66 @@ def make_files( fpath, fname):
     setup.write(verbose=False)
 
     #Writes a MAPTEXT file, used to add more information about each eruption to gifs.
-    #Should have file nameing conention: MAPTEXT.'pid'
+    #Should have file nameing convention: MAPTEXT.'pid'
     #Contains lines of text
-    
     with open(wdir + 'MAPTEXT.CFG', 'w') as fid:
-        fid.write('HYSPLIT Trajectory Calculation for '+volcname+' Volcano\n')
+        fid.write('HYSPLIT Trajectory Calculation for '+volcname2+' Volcano\n')
         fid.write('Line2 \n')
         fid.write('Lat: '+str(start_lat)+'    Lon: '+str(start_lon)+'    Vent Height: '+str(alt)+' m\n')
-        fid.write('Alert Type: '+data[0]+'     VAAC: '+str(vaac)+'\n')
+        fid.write('Alert Type: '+data[0]+'    VAAC: '+str(vaac)+'\n')
         current=dtime.utcnow()
         fid.write('Job Start: '+current.strftime('%B %d, %Y')+' at '+current.strftime('%H:%M:%S')+' UTC \n')
         fid.close()
-    
-    return wdir, pid
+
+    #Identify ash cloud properties - create dispersion files
+    #if (alert_type == 'ash'):
+    if (alert_type == 'x'):
+        hgts = volcalert.get_height(alerts)
+        mass = volcalert.get_mass(alerts)
+        radius = volcalert.get_radius(alerts)
+        area = volcalert.get_area(alerts)
+        #Number of days for met fields needed - based on hour of trajectories
+        if duration2 > 0:
+            ndays2 = (duration2 // 24) + 1
+            edate2 = sdate + td(days = ndays2)
+        #met = fd.findcycles_archive(sdate,edate,met_model,'Forward')
+            met = fd.findcycles_forecast(sdate,met_model)
+        if duration2 < 0:
+            ndays2 = (duration2 // 24)
+            edate2 = sdate + td(days = ndays2)
+            met = fd.findcycles_archive(sdate,edate2,met_model,'Back')
+        #Write CONTROL file
+        control2 = hcontrol.HycsControl(fname='CONTROL.disp' + str(pid), working_directory = wdir2)
+        #simulation start date
+        control2.add_sdate(sdate)
+        #set the duration of the simulation (hours)
+        control2.run_duration = duration2
+        #set location of volcano
+        control2.add_location(latlon = (start_lat, start_lon), alt = alt)
+        control2.add_location(latlon = (start_lat, start_lon), alt = hgts)
+        control2.vertical_motion = vertical
+        control2.ztop = topbound
+        control2.outdir = wdir2
+        control2.outfile = 'cdump.'+str(pid)
+        #set particle sizes
+        control2.num_sp = 4   #4 total particles (default values will be used)
+        control2.species = ['p006', 'p020', 'p060', 'p200']
+        # add met files
+        for metdir, metfile in zip(met[0],met[1]):
+            control2.add_metfile(metdir, metfile)
+        control2.write(annotate = False)
+        
+    return wdir, pid, wdir2
 
 def run_hysp(wdir, pid):
     """ Launches HYSPLIT using CONTROL and SETUP files created in make_files.
     Prints current working directory."""
     import os
-
     cwd=os.getcwd()
     os.chdir(wdir)
     cwd2=os.getcwd()
 #    print('Current wdir is: '+cwd2)
     os.system('/hysplit-users/allisonr/HYSPLIT/exec/hyts_std ' +pid)
-
     return 'HYSPLIT run for '+pid+' is completed!'
 
 def make_traj(wdir, pid):
@@ -142,14 +186,11 @@ def make_traj(wdir, pid):
     #process = subprocess.Popen('/hysplit-users/allisonr/hysplit/trunk/exec/trajplot -itdump.'+pid+' -l6 -otraj_'+pid+'.ps -s1 -v1')
     import os
     from monet.utilhysplit.hysplit_graf import trajplot 
-    
     cwd=os.getcwd()
     os.chdir(wdir)
-    #print(wdir)
     #os.system('python /hysplit-users/allisonr/Alice/MONET/monet/utilhysplit/hysplit_graf/trajplot.py -itdump.'+pid+' -otraj_'+pid+'.ps')
     os.system('/hysplit-users/allisonr/HYSPLIT/exec/trajplot -itdump.'+pid+' -otraj_'+pid+'.ps -a3 -A1 -l6 -s1 -v1 -k2 +n')
     os.system('cp MAPTEXT.CFG MAPTEXT.'+pid)
-
     return 'Figure called traj_'+pid+'.ps is created!'
     
 def compress_kml(wdir, pid):
@@ -158,7 +199,6 @@ def compress_kml(wdir, pid):
     cwd = os.getcwd()
     os.chdir(wdir)
     os.system('zip traj_'+pid+'.kmz traj_'+pid+'_01.kml')
-
     return 'kmz file called traj_'+pid+'.kmz is created!'
 
 def check_for_figs(wdir, pid):
@@ -167,6 +207,5 @@ def check_for_figs(wdir, pid):
     cwd = os.getcwd()
     os.chdir(wdir)
     trajfile = os.path.isfile('traj_'+pid+'.ps')
-
     return trajfile
 
